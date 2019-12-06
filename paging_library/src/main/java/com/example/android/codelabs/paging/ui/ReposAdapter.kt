8f244ecefa69957202799b367fca12ad1f6bc75a
songguotao <songguotao@le.com>
2019-12-06 10:54:12 +0800

    Make the RecyclerView Adapter work with a PagedList,修改 RecyclerView 的 Adapter，从 ListAdapter 变为 PagedListAdapter。

--- paging_library/src/main/java/com/example/android/codelabs/paging/ui/ReposAdapter.kt
+++ paging_library/src/main/java/com/example/android/codelabs/paging/ui/ReposAdapter.kt
@@ -17,20 +17,27 @@
 package com.example.android.codelabs.paging.ui
 
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import com.example.android.codelabs.paging.model.Repo
 
 /**
  * Adapter for the list of repositories.
  */
-class ReposAdapter : ListAdapter<Repo, androidx.recyclerview.widget.RecyclerView.ViewHolder>(REPO_COMPARATOR) {
+class ReposAdapter :
+    PagedListAdapter<Repo, androidx.recyclerview.widget.RecyclerView.ViewHolder>(REPO_COMPARATOR) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): androidx.recyclerview.widget.RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): androidx.recyclerview.widget.RecyclerView.ViewHolder {
         return RepoViewHolder.create(parent)
     }
 
-    override fun onBindViewHolder(holder: androidx.recyclerview.widget.RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: androidx.recyclerview.widget.RecyclerView.ViewHolder,
+        position: Int
+    ) {
         val repoItem = getItem(position)
         if (repoItem != null) {
             (holder as RepoViewHolder).bind(repoItem)
@@ -40,10 +47,10 @@ class ReposAdapter : ListAdapter<Repo, androidx.recyclerview.widget.RecyclerView
     companion object {
         private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<Repo>() {
             override fun areItemsTheSame(oldItem: Repo, newItem: Repo): Boolean =
-                    oldItem.fullName == newItem.fullName
+                oldItem.fullName == newItem.fullName
 
             override fun areContentsTheSame(oldItem: Repo, newItem: Repo): Boolean =
-                    oldItem == newItem
+                oldItem == newItem
         }
     }
 }
